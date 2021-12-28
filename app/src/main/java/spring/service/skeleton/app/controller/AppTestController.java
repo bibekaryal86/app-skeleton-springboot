ccb33b249a790e744a151364964282253aaba32c
Bibek Aryal <bibekaryal86@gmail.com>
2021-12-28 12:00:10 -0700

    Add Controller with an Endpoint for Ping

---
+++ app/src/main/java/spring/service/skeleton/app/controller/AppTestController.java
@@ -0,0 +1,16 @@
+package spring.service.skeleton.app.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppTestController {
+    @CrossOrigin
+    @GetMapping(value = "/tests/ping", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> pingTest() {
+        return ResponseEntity.ok("{\"ping\": \"successful\"}");
+    }
+}
