b0dc7cfb3fe210741fa3be9cef3353efe5019fe0
Bibek Aryal <34973169+bibekaryal86@users.noreply.github.com>
2021-12-28 00:38:26 -0700

    Update Custom Actuator Endpoint to Return JSON

--- app/src/main/java/spring/service/skeleton/app/util/ActuatorUtilsClearCacheEndpoint.java
+++ app/src/main/java/spring/service/skeleton/app/util/ActuatorUtilsClearCacheEndpoint.java
@@ -1,13 +1,14 @@
-package spring.service.skeleton.app.util;
+package health.data.java.app.util;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.boot.actuate.endpoint.annotation.Selector;
 import org.springframework.cache.CacheManager;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
-import static java.util.Objects.requireNonNull;
+import java.util.Objects;
 
 @Slf4j
 @Component
@@ -21,17 +22,17 @@ public class ActuatorUtilsClearCacheEndpoint {
     }
 
     @ReadOperation
-    public String clearCaches() {
+    public ResponseEntity<String> clearCaches() {
         log.info("Firing Clear Caches Actuator!!!");
         cacheManager.getCacheNames()
-                .forEach(cacheName -> requireNonNull(cacheManager.getCache(cacheName)).clear());
-        return "Finished Clear Caches Actuator!!!";
+                .forEach(cacheName -> Objects.requireNonNull(cacheManager.getCache(cacheName)).clear());
+        return ResponseEntity.ok("{\"clearCaches\": \"successful\"}");
     }
 
     @ReadOperation
-    public String clearCache(@Selector String cacheNameSelector) {
+    public ResponseEntity<String> clearCache(@Selector String cacheNameSelector) {
         log.info("Firing Clear Cache Actuator: {} !!!", cacheNameSelector);
-        requireNonNull(cacheManager.getCache(cacheNameSelector)).clear();
-        return String.format("Finished Clear Cache Actuator: %s !!!", cacheNameSelector);
+        Objects.requireNonNull(cacheManager.getCache(cacheNameSelector)).clear();
+        return ResponseEntity.ok("{\"clearCache\": \"successful\"}");
     }
 }
