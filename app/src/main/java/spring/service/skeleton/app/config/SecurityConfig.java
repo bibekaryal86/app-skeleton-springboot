02d876dd21a172e09cde7310c7c4ef43c13668c5
Bibek Aryal <bibekaryal86@gmail.com>
2022-12-11 22:10:12 -0800

    Update Java, Gradle and Spring Security

--- app/src/main/java/spring/service/skeleton/app/config/SecurityConfig.java
+++ app/src/main/java/spring/service/skeleton/app/config/SecurityConfig.java
@@ -26,8 +26,6 @@ public class SecurityConfig {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .csrf()
-                .disable()
                 .httpBasic()
                 .and()
                 .sessionManagement()
